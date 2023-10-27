91826a99ffa51607ff072dd4d8d4de756adf07dc
crisdev88 <143648868+crisDev175@users.noreply.github.com>
2023-10-27 15:54:48 -0600

    config db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app from './app.js'
+import connectDB from './db.js'
 const port = 8000;
 
+
+connectDB();
 app.listen(port)
-console.log('Server listening on port localhost:'+ port)
+console.log('Server listening on port localhost:' + port)
